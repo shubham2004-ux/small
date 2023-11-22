3ae1846c9540c5025d6e7b91f0ee9032dd6e5849
SHUBHAM SINGH <shubham.0202.in@gmail.com>
2023-11-22 19:37:58 +0530

    oj

---
+++ script.js
@@ -0,0 +1,130 @@
+// // function toggleDarkMode() {
+// //     const shubham = document.body;
+// //     shubham.classList.toggle("dark-mode");
+// // }
+
+// // function handleClick() {
+// //     const shub = document.getElementById('FIRST')
+// //     shub.classList.toggle('blue-mode')
+// // }
+
+
+// let a = 6
+
+// let vanila = a < 10 && a > 5 ? true : false;
+// console.log(vanila)
+
+// function Switch() {
+//     document.body.classList.toggle('dark-mode');
+
+
+// }
+
+// let text1 = "Hello World!";
+// let result = text1.toUpperCase();
+// console.log(result)
+
+
+// function text() {
+//     let b = document.getElementById('texttutils')
+//     b.toUpperCase()
+// }
+// const handleClick = () => {
+//     text()
+// }
+
+// function handleClick() {
+//     let a = document.getElementById('texttutils')
+//     a.classList.toggle('textutils')
+// }
+
+// let d = true
+// const handleChange = () => {
+//     let mona = document.getElementsByTagName('p')[0];
+//     d == true ? mona.style.background = "blue" : mona.style.background = "red";
+// }
+
+// const red = () => {
+//     handleChange()
+// }
+
+// console.log(document.getElementsByTagName('p')[0])
+
+
+
+
+// uppercase1 = () => {;
+//     console.log(document.getElementById('ok'))
+//     let a = document.getElementById('ok')
+
+
+//     a.classList.toggle('pk')
+
+
+
+// }
+
+// const shubham = {
+//     name: 'mansi',
+//     age: '16',
+//     address: 'narela',
+//     no: '4566566165'
+
+
+// }
+
+// const bca = {
+//     name: 'ashwin',
+//     course: 'bca',
+//     contact: '84666166',
+//     age: '20'
+// }
+
+// const bca2 = {
+//     name: 'p',
+//     age: '14'
+
+// }
+
+
+// let a = [bca2, bca, shubham]
+
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a)
+// }
+
+// let a = 'SHUBHAM'
+
+// let b = 'ASHWIN'
+
+// console.log(`Now ${a} and ${b} are two boths friends`)
+// console.log("hello boths of you" + a, +b)
+
+
+
+
+
+
+
+
+
+
+// setTimeout(() => {
+//     alert('the shubham name is printing')
+
+
+//     setTimeout(() => {
+//         for (let i = 0; i < a; i++) {
+//             document.write('Hello Shubham')
+
+//         }
+
+
+//     }, 5000)
+
+
+// }, 1000)
+
+// let a = document.getElementById('pk')
+
+// a.classList.contains('pulse-Element')
